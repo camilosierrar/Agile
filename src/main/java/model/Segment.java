84cfc4843f53059d6ded666728b0435b8884bf11
Andrieu Girard <andrieu0407@gmail.com>
2020-11-17 11:57:42 +0100

    Création de la base de classe segment

--- src/main/java/model/Segment.java
+++ src/main/java/model/Segment.java
@@ -1,4 +1,51 @@
 package model;
 
 public class Segment {
+
+    //Attribute folder
+    private Intersection origin;
+    private Intersection destination;
+    private String name;
+    private double length;
+
+    //Constructor folder
+    public Segment(Intersection origin, Intersection destination, String name, double length) {
+        this.origin = origin;
+        this.destination = destination;
+        this.name = name;
+        this.length = length;
+    }
+
+    //Getters-Setters folder
+    public Intersection getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Intersection origin) {
+        this.origin = origin;
+    }
+
+    public Intersection getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Intersection destination) {
+        this.destination = destination;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
 }
