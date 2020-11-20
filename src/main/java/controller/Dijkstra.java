81dc059955390f1b29c502e13aba84590e886f49
b3nker <benja@DESKTOP-192F4IV>
2020-11-20 11:40:02 +0100

    adds Dijkstra

---
+++ src/main/java/controller/Dijkstra.java
@@ -0,0 +1,78 @@
+package controller;
+
+import model.Intersection;
+import model.Segment;
+
+import java.util.*;
+
+public class Dijkstra {
+    private Set<Node> nodes = new HashSet<>();
+
+    public void addNode(Node nodeA) {
+        nodes.add(nodeA);
+    }
+
+    public static Dijkstra calculateShortestPathFromSource(Dijkstra graph, Node source) {
+        source.setDistance(0);
+
+        Set<Node> settledNodes = new HashSet<>();
+        Set<Node> unsettledNodes = new HashSet<>();
+
+        unsettledNodes.add(source);
+
+        while (unsettledNodes.size() != 0) {
+            Node currentNode = getLowestDistanceNode(unsettledNodes);
+            unsettledNodes.remove(currentNode);
+            for (Map.Entry< Node, Integer> adjacencyPair:
+                    currentNode.getAdjacentNodes().entrySet()) {
+                Node adjacentNode = adjacencyPair.getKey();
+                Integer edgeWeight = adjacencyPair.getValue();
+                if (!settledNodes.contains(adjacentNode)) {
+                    calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
+                    unsettledNodes.add(adjacentNode);
+                }
+            }
+            settledNodes.add(currentNode);
+        }
+        return graph;
+    }
+
+    /**
+     *  renvoie le nœud dont la distance est la plus basse du jeu de nœuds non définis
+     * @param unsettledNodes
+     * @return
+     */
+    private static Node getLowestDistanceNode(Set < Node > unsettledNodes) {
+        Node lowestDistanceNode = null;
+        int lowestDistance = Integer.MAX_VALUE;
+        for (Node node: unsettledNodes) {
+            int nodeDistance = node.getDistance();
+            if (nodeDistance < lowestDistance) {
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = node;
+            }
+        }
+        return lowestDistanceNode;
+    }
+
+    /**
+     * compare la distance réelle à celle nouvellement calculée tout en suivant le chemin récemment exploré
+     * @param evaluationNode
+     * @param edgeWeigh
+     * @param sourceNode
+     */
+    private static void calculateMinimumDistance(Node evaluationNode,
+                                                 Integer edgeWeigh, Node sourceNode) {
+        Integer sourceDistance = sourceNode.getDistance();
+        if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
+            evaluationNode.setDistance(sourceDistance + edgeWeigh);
+            LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+            shortestPath.add(sourceNode);
+            evaluationNode.setShortestPath(shortestPath);
+        }
+    }
+
+    public Set<Node> getNodes() {
+        return nodes;
+    }
+}
--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,8 +1,49 @@
 package main;
 
+import controller.Dijkstra;
+import controller.Node;
+
 public class main {
     public static void main (String[] args){
         System.out.println("Bye world");
-        Gui gui = new Gui();
+       // Gui gui = new Gui();
+
+
+
+        //TEST
+        Node nodeA = new Node(1);
+        Node nodeB = new Node(2);
+        Node nodeC = new Node(3);
+        Node nodeD = new Node(4);
+        Node nodeE = new Node(5);
+        Node nodeF = new Node(6);
+
+        nodeA.addDestination(nodeB, 10);
+        nodeA.addDestination(nodeC, 15);
+
+        nodeB.addDestination(nodeD, 12);
+        nodeB.addDestination(nodeF, 15);
+
+        nodeC.addDestination(nodeE, 10);
+
+        nodeD.addDestination(nodeE, 2);
+        nodeD.addDestination(nodeF, 1);
+
+        nodeF.addDestination(nodeE, 5);
+
+        Dijkstra graph = new Dijkstra();
+
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+        graph.addNode(nodeD);
+        graph.addNode(nodeE);
+        graph.addNode(nodeF);
+
+        graph = Dijkstra.calculateShortestPathFromSource(graph, nodeA);
+        for(Node res: graph.getNodes()){
+            System.out.println(res.getDistance());
+        }
+        //END TEST
     }
 }
