5afeebcc09ac57c28e12ddd8111c8fd692bc81dc
Camilo <camilo.sierra.r@gmail.com>
2020-11-20 11:13:06 +0100

    GUI

--- src/main/java/main/Gui.java
+++ src/main/java/main/Gui.java
@@ -7,32 +7,76 @@ public class Gui extends JFrame {
     //Declaration des attributs de la fenetre (private tata bla;)
 
     //Declaration des elemants graphiques de la fenetre (boutons)
+    JPanel base;
+    JPanel map;
+    JPanel info;
+    JPanel topBar;
+    JButton mapRead;
+    JButton reqRead;
+    JButton getBestTour;
+    JTextField mapPath;
+    JTextField reqPath;
 
     //Constructor
     public Gui() {
         //window name
-        super("Title");
+        super("Delivelov");
 
         //Dimensions et layout
-        this.setSize(800,800);
+        this.setSize(1000,600);
+        this.setMinimumSize(new Dimension(1200, 600));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         this.setLayout(new BorderLayout()); //ou FlowLayout()
 
         //Panels
-        JPanel base = new JPanel(new BorderLayout()); // ou FlowLayout()
-        JPanel map = new JPanel();
-        JPanel sidebar = new JPanel();
+        base = new JPanel(new BorderLayout()); // ou FlowLayout()
+        topBar = new JPanel();
+        map = new JPanel();
+        info = new JPanel();
 
-        //Attributes (bla= new tata())
+        //JLabel
+        JLabel mapReadLabel = new JLabel("Path To Map");
+        JLabel reqReadLabel = new JLabel("Path To Requests");
+        JLabel temp = new JLabel("If you Click on the Map you will receive information here");
+        temp.setForeground(Color.WHITE);
+        mapReadLabel.setForeground(Color.WHITE);
+        reqReadLabel.setForeground(Color.WHITE);
+
+        //JTextField
+        mapPath = new JTextField(20);
+        reqPath = new JTextField(20);
+
+        //Buttons
+        mapRead = new JButton("Load Map");
+        reqRead = new JButton("Load Requests");
+        getBestTour = new JButton("Find Best Tour");
+
+        //Attributes
         base.setBackground(Color.DARK_GRAY);
-        map.setBackground(Color.CYAN);
-        sidebar.setBackground(Color.BLUE);
-        //Add
+        map.setBackground(new Color(0,51,102));
+        topBar.setBackground(Color.BLACK);
+        info.setBackground(Color.DARK_GRAY);
+        info.setMaximumSize(new Dimension(300,Integer.MAX_VALUE));
+        //Add to topBar
+            //MapReading
+        topBar.add(mapReadLabel);
+        topBar.add(mapPath);
+        topBar.add(mapRead);
+            //ReqReading
+        topBar.add(reqReadLabel);
+        topBar.add(reqPath);
+        topBar.add(reqRead);
+            //getBestTour
+        topBar.add(getBestTour);
+
+        //Add to info
+        info.add(temp);
+
+        //Add panels
         base.add(map,BorderLayout.CENTER);
-        base.add(sidebar, BorderLayout.WEST);
+        base.add(topBar, BorderLayout.PAGE_START);
+        base.add(info, BorderLayout.WEST);
         this.add(base);
-        //bouton.addActionListener(new Listener(this,#));
 
         //END of Constructor
         this.setVisible(true);
