3938be602b383292386abcee63ecc9daeda544d6
Javier Galarza <javier.galarza12@gmail.com>
2020-11-24 10:01:02 +0100

    Created action listeners

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -7,8 +7,6 @@ import java.awt.*;
 public class main {
     public static void main (String[] args){
         System.out.println("Bye world");
-        //XMLmap.readData();
-        //XMLrequest.readData();
         Controller controller = new Controller();
         Gui gui = new Gui(controller);
     }
